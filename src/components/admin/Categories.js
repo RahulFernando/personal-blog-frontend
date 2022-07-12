c917ae3b100285ef50fd07b5ea2b4251a951c6d5
RahulFernando <rahulf@itx360.com>
2022-07-12 12:07:12 +0530

    feat: categories fetch api bind

--- src/components/admin/Categories.js
+++ src/components/admin/Categories.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Grid, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
+// actions
+import { fetchCategories } from "../../reducers/categorySlice";
+
 const columns = [
   {
-    field: "category",
+    field: "name",
     headerName: "Category",
     flex: 1,
   },
@@ -31,6 +35,15 @@ const columns = [
 ];
 
 const Categories = () => {
+  const dispatch = useDispatch();
+
+  const categories = useSelector((state) => state.category.fetchCategoryData.data);
+  const loading = useSelector((state) => state.category.fetchCategoryData.loading);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
@@ -40,7 +53,14 @@ const Categories = () => {
       </Grid>
       <Grid item xs={6} />
       <Grid item xs={12}>
-        <DataGrid columns={columns} rows={[]} sx={{ minHeight: '160px' }} hideFooter />
+        <DataGrid
+          columns={columns}
+          rows={categories}
+          getRowId={(row) => row._id}
+          loading={loading}
+          sx={{ minHeight: "160px" }}
+          hideFooter
+        />
       </Grid>
     </Grid>
   );
