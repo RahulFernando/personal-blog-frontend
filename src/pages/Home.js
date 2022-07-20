da1f82f86d386637881d5f7e83df3022e8bc4a97
RahulFernando <rahulf@itx360.com>
2022-07-20 09:12:37 +0530

    fix: post content fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { Grid } from "@mui/material";
 
 // components
 import Card from "../components/card/Card";
 
 const Home = () => {
-  const posts = useSelector(state => state.posts.fetchPostData.data);
+  const posts = useSelector((state) => state.posts.fetchPostData.data);
+
   return (
     <Grid container spacing={2} mt={2}>
       {posts.map((post) => (
         <Grid key={post._id} item xs={3}>
-          <Card image={post.image} title={post.title} content={post.content} />
+          <Card
+            image={post.image}
+            title={post.title}
+            content={post.content}
+          />
         </Grid>
       ))}
     </Grid>
